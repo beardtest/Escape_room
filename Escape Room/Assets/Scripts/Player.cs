b0cd5c34dd3055beab7238fc5e6096031da43c13
Myles <myles.hunt3850@gmail.com>
2020-06-29 23:03:06 +0100

    Added a crude jump
    its a bit OP

--- Escape Room/Assets/Scripts/Player.cs
+++ Escape Room/Assets/Scripts/Player.cs
@@ -6,17 +6,20 @@ public class Player : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    private Collider2D coll;
 
     [SerializeField] private float speed = 5;
+    [SerializeField] private LayerMask ground;
 
+    private float jumpForce = 20;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
 
-        
     }
 
     // Update is called once per frame
@@ -30,11 +33,17 @@ public class Player : MonoBehaviour
     {
         var deadZone = 0.25;
 
-        if (Input.GetAxis("Horizontal") < -deadZone)
+        if (Input.GetAxis("Jump") > 0.1 && coll.IsTouchingLayers(ground))
+        {
+            Jump();
+        }
+
+        else if (Input.GetAxis("Horizontal") < -deadZone)
         {
 
             rb.velocity = new Vector2(-speed, rb.velocity.y);
             //transform.localScale = new Vector2(-1, 1);
+
 
         }
 
@@ -52,5 +61,10 @@ public class Player : MonoBehaviour
             rb.velocity = Vector2.zero;
 
         }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 }
